a984ab134051c77d36adea90851c3b643e6cee91
Zachary Djerdjouri <zdjer050@uottawa.ca>
2025-08-14 22:09:18 -0400

    Added upload funcitonality

--- backend/src/main/java/com/amigoscode/s3/S3Service.java
+++ backend/src/main/java/com/amigoscode/s3/S3Service.java
@@ -1,7 +1,9 @@
 package com.amigoscode.s3;
 
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3Service {
@@ -10,6 +12,15 @@ public class S3Service {
 
     public S3Service(S3Client s3Client){
         this.s3Client = s3Client;
+    }
+
+    public void putObject(String bucketName, String key, byte[] file){
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.putObject(objectRequest, RequestBody.fromBytes(file));
     }
 
 
